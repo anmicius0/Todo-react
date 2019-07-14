2be927b37118296f694e89dccbd0183d3f73f89b
cg <allenlin@disroot.org>
2019-07-14 15:27:53 +0800

    add Swal back to code

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,9 +2,45 @@ import React, { createRef } from "react";
 
 import PropTypes from "prop-types";
 import TodoItem from "./TodoItem";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
+// use Swal to get todo message
+async function getTodo() {
+  const { value: message } = await MySwal.fire({
+    title: "Enter",
+    input: "text",
+    showCancelButton: true,
+    inputPlaceholder: "type in your todo!"
+  });
+  if (message !== undefined) {
+    return message;
+  }
+}
+
+function successAlert() {
+  MySwal.fire({
+    position: "top-end",
+    toast: true,
+    type: "success",
+    title: "Success!",
+    timer: 3000,
+    showConfirmButton: false
+  });
+}
 
 const Main = props => {
-  const input = createRef();
+  // get message from Swal and add to state(todos)
+  const setTodos = () => {
+    getTodo().then(message => {
+      if (message) {
+        props.addTodos(message);
+        successAlert();
+      }
+    });
+  };
 
   return (
     <main>
@@ -21,15 +57,7 @@ const Main = props => {
         ))}
       </div>
 
-      <input type="text" ref={input} />
-
-      <div
-        onClick={() => {
-          props.addTodos(input.current.value);
-          input.current.value = "";
-        }}
-        id="btn"
-      >
+      <div onClick={() => setTodos()} id="btn">
         <button className="icon-btn add-btn">
           <div className="add-icon" />
           <div className="btn-txt">Add</div>
